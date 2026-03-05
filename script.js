e26324dbbf2acf2c30aff32f6a20499d8d0d29e8
mfdesgin <oh.sgdesign@gmail.com>
2026-03-06 01:27:54 +0900

    [협업] : 세 번째 글 추가

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const posts = [
   { title: '첫 번째 글', date: '2026-03-05', content: '바닐라 JS로 블로그 만들기 시작!' },
   { title: '두 번째 글', date: '2026-03-04', content: '오늘은 CSS 스트로크 공부를 했다.' },
+  { title: '세 번째 글', date: '2026-03-03', content: 'JavaScript의 closure 개념을 공부했다.' },
 ];
 
 // 화면에 글 목록을 뿌려주는 함수
